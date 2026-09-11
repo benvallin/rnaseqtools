6c18906abc0c6d5f7119c9666ea5db690af884ee
Benjamin Vallin <ben.vallin@gmail.com>
2026-09-11 14:10:14 +0100

    update

--- R/make_transcript_metadata.R
+++ R/make_transcript_metadata.R
@@ -30,16 +30,16 @@
 #'
 make_transcript_metadata <- function(input) {
 
-  keep <- c("transcript_id", "transcript_type", "gene_id", "gene_name")
+  keep <- c("transcript_id", "gene_id", "gene_name", "transcript_type", "gene_type")
 
   if(!is.data.frame(input) || !all(keep %in% colnames(input))) {
-    stop("Input must be a tibble with columns \"transcript_id\", \"transcript_type\", \"gene_id\" and \"gene_name\".",
+    stop("Input must be a tibble with columns \"transcript_id\", \"gene_id\", \"gene_name\", \"transcript_type\", and \"gene_type\".",
          call. = F)
   }
 
   output <- unique(input[!is.na(input$transcript_id), keep])
 
-  colnames(output) <- c("ensembl_transcript_id_version", "transcript_type", "ensembl_gene_id_version", "gene_symbol")
+  colnames(output) <- c("ensembl_transcript_id_version", "ensembl_gene_id_version", "gene_symbol", "transcript_type", "gene_type")
 
   output
 
